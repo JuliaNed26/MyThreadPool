6769b112cc4319b1653cb71ab723e05cdab83951
Yuliia Nedavnia <nedavnaia@gmail.com>
2025-05-12 19:52:45 +0300

    Thread pool for simple synchronous tasks (BlockingCollection<Action>)

---
+++ MyThreadPool/MyThreadPool/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ThreadPool = MyThreadPool.ThreadPool;
+
+var threadPool = new ThreadPool();
+threadPool.Schedule(() => Console.WriteLine("Buenas tardes!"));
+threadPool.Schedule(() => Console.WriteLine("Me illamo Julia"));
+threadPool.Schedule(() => Console.WriteLine("Yo ablo Espanol"));
+threadPool.StopAdding();
---
+++ MyThreadPool/MyThreadPool/ThreadPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+namespace MyThreadPool;
+
+public class ThreadPool
+{
+    private readonly BlockingCollection<Action> _poolOfTasks;
+
+    public ThreadPool()
+    {
+        _poolOfTasks = new BlockingCollection<Action>(new ConcurrentQueue<Action>());
+        var executionThread = new Thread(ExecuteAsync);
+        executionThread.Start();
+    }
+
+    public void Schedule(Action taskToExecute)
+    {
+        _poolOfTasks.Add(taskToExecute);
+    }
+
+    public void StopAdding()
+    {
+        _poolOfTasks.CompleteAdding();
+    }
+
+    private void ExecuteAsync()
+    {
+        foreach (var action in _poolOfTasks.GetConsumingEnumerable())
+        {
+            action.Invoke();
+        }
+    }
+}

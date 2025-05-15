77b563ed2bc4af98be2937d18344253b7d534740
Yuliia Nedavnia <nedavnaia@gmail.com>
2025-05-15 19:57:35 +0300

    ThreadPool for asynchronous operations

--- MyThreadPool/MyThreadPool/Program.cs
+++ MyThreadPool/MyThreadPool/Program.cs
@@ -3,7 +3,22 @@
 using ThreadPool = MyThreadPool.ThreadPool;
 
 var threadPool = new ThreadPool();
-threadPool.Schedule(() => Console.WriteLine("Buenas tardes!"));
-threadPool.Schedule(() => Console.WriteLine("Me illamo Julia"));
-threadPool.Schedule(() => Console.WriteLine("Yo ablo Espanol"));
-threadPool.StopAdding();
+threadPool.Schedule(() => new Task(() =>
+{
+    Console.WriteLine("Buenas tardes!");
+    Thread.Sleep(100);
+}));
+
+threadPool.Schedule(() => new Task(() =>
+{
+    Console.WriteLine("Me llamo Julia");
+    Thread.Sleep(50);
+}));
+
+threadPool.Schedule(() => new Task(() =>
+{
+    Console.WriteLine("Yo ablo Espanol");
+    Thread.Sleep(30);
+}));
+
+await threadPool.StopAddingAsync();
--- MyThreadPool/MyThreadPool/ThreadPool.cs
+++ MyThreadPool/MyThreadPool/ThreadPool.cs
@@ -4,30 +4,40 @@ namespace MyThreadPool;
 
 public class ThreadPool
 {
-    private readonly BlockingCollection<Action> _poolOfTasks;
+    private readonly Task _processingTask;
+    private readonly BlockingCollection<Func<Task>> _poolOfTasks;
 
     public ThreadPool()
     {
-        _poolOfTasks = new BlockingCollection<Action>(new ConcurrentQueue<Action>());
-        var executionThread = new Thread(ExecuteAsync);
-        executionThread.Start();
+        _poolOfTasks = new BlockingCollection<Func<Task>>(new ConcurrentQueue<Func<Task>>());
+        _processingTask = Task.Run(ExecuteAsync);
     }
 
-    public void Schedule(Action taskToExecute)
+    public void Schedule(Func<Task> taskToExecute)
     {
         _poolOfTasks.Add(taskToExecute);
     }
 
-    public void StopAdding()
+    public async Task StopAddingAsync()
     {
         _poolOfTasks.CompleteAdding();
+        await _processingTask;
     }
 
-    private void ExecuteAsync()
+    private async Task ExecuteAsync()
     {
-        foreach (var action in _poolOfTasks.GetConsumingEnumerable())
+        var tasks = new List<Task>();
+        foreach (var task in _poolOfTasks.GetConsumingEnumerable())
         {
-            action.Invoke();
+            var taskToExecute = task();
+            if (taskToExecute.Status == TaskStatus.Created)
+            {
+                taskToExecute.Start();
+            }
+
+            tasks.Add(taskToExecute); 
         }
+
+        await Task.WhenAll(tasks);
     }
 }
